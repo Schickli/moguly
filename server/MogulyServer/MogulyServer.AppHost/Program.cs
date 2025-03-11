8bafb8287e544efbc8ce6695521148e795155090
Damjan Djukic <dami24079@gmail.com>
2025-03-11 18:14:08 +0100

    exposed endpoints

--- server/MogulyServer/MogulyServer.AppHost/Program.cs
+++ server/MogulyServer/MogulyServer.AppHost/Program.cs
@@ -1,5 +1,6 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
-var apiService = builder.AddProject<Projects.MogulyServer_Api>("moguly-api");
+var apiService = builder.AddProject<Projects.MogulyServer_Api>("moguly-api")
+    .WithExternalHttpEndpoints();
 
 builder.Build().Run();
