1947edf845078147020a0ec0f4b4844f0d9c0826
Damjan Djukic <dami24079@gmail.com>
2025-03-11 22:27:58 +0100

    added MediatR pattern

--- server/MogulyServer/MogulyServer.Signal/Feature/Moguly/MogulyHub.cs
+++ server/MogulyServer/MogulyServer.Signal/Feature/Moguly/MogulyHub.cs
@@ -1,9 +1,18 @@
-﻿using Microsoft.AspNetCore.SignalR;
+﻿using MediatR;
+using Microsoft.AspNetCore.SignalR;
+using MogulyServer.Signal.Feature.JoinGame;
 
 namespace MogulyServer.Signal.Feature.Moguly
 {
     public class MogulyHub : Hub<IMogulyClient>
     {
+        private readonly IMediator _mediator;
+
+        public MogulyHub(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
         public override async Task OnConnectedAsync()
         {
             await Clients.All.ReceiveMessage($"{Context.ConnectionId}", "has joined");
@@ -12,6 +21,24 @@ namespace MogulyServer.Signal.Feature.Moguly
         public async Task SendToAll(string message)
         {
             await Clients.All.ReceiveMessage($"{Context.ConnectionId}", $"{message}");
+        }
+
+        public async Task JoinGame(Guid gameId)
+        {
+            var playerConnectionId = Context.ConnectionId; // CLARIFY: maybe use userIdentifier????
+
+            var cmd = new JoinGameCommand()
+            {
+                GameId = gameId,
+                PlayerConnectionId = playerConnectionId
+            };
+
+            await _mediator.Send(cmd);
+        }
+
+        public async Task PingPlayers(Guid gameId)
+        {
+            await Clients.Group(gameId.ToString()).ReceiveMessage("TEST", $"Your are in grou {gameId}");
         }
     }
 }
