6e25e391343d20ebdff0b4bc0198119976cc3500
Damjan Djukic <dami24079@gmail.com>
2025-03-12 19:13:29 +0100

    added Square class

---
+++ server/MogulyServer/MogulyServer.Domain/Square/ColoredStreets/StreetColor.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MogulyServer.Domain.Square.ColoredStreets
+{
+    public record StreetColor
+    {
+        public static readonly StreetColor Brown = new("Brown");
+        public static readonly StreetColor LightBlue = new("LightBlue");
+        public static readonly StreetColor Pink = new("Pink");
+        public static readonly StreetColor Orange = new("Orange");
+        public static readonly StreetColor Red = new("Red");
+        public static readonly StreetColor Yellow = new("Yellow");
+        public static readonly StreetColor Green = new("Green");
+        public static readonly StreetColor DarkBlue = new("DarkBlue");
+
+        private readonly string _color;
+        private StreetColor(string color)
+        {
+            _color = color;
+        }
+    }
+}
