c2c553d75a6c2ee5317cdc9b898b70939c76162b
Maurin <67188361+Schickli@users.noreply.github.com>
2025-03-12 17:49:03 +0000

    Handle Logout

--- moguly/src/components/navbar.tsx
+++ moguly/src/components/navbar.tsx
@@ -2,6 +2,7 @@ import { twMerge } from "tailwind-merge";
 import { ModeToggle } from "./theming/mode-toggle";
 import { Button } from "./ui/button";
 import { LogOut } from "lucide-react";
+import { useNavigate } from "@tanstack/react-router";
 
 interface NavbarProps {
   gameId?: string;
@@ -9,6 +10,13 @@ interface NavbarProps {
 }
 
 export function NavBar({ className, gameId }: NavbarProps) {
+  const navigate = useNavigate();
+  const handleLogOut = () => {
+    // handle log out
+
+    navigate({ to: "/" });
+  };
+
   return (
     <nav className={twMerge(className, "justify-end flex p-4 w-full shadow")}>
       <div className="flex gap-2">
@@ -17,7 +25,8 @@ export function NavBar({ className, gameId }: NavbarProps) {
           <Button
             variant="outline"
             size="icon"
-            className={"hover:text-destructive"}
+            className="hover:text-destructive"
+            onClick={() => handleLogOut()}
           >
             <LogOut />
           </Button>
